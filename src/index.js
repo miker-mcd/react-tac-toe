c92215a61cb4678843209c1dae3e9cc1bbc6b0c6
miker-mcd <michaelrmcd@gmail.com>
2018-03-26 17:12:46 -0700

    Test master branch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// test
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
